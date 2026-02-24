bf363eee7b341dfe0c4340e557cc461d8d6a617b
Dimasaurus <galihdimas@outlook.com>
2026-02-24 11:29:29 +0700

    update Fitur detail All Project

--- src/app/api/project/[slug]/route.ts
+++ src/app/api/project/[slug]/route.ts
@@ -1,4 +1,4 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import { supabaseServer } from "@/app/lib/supabase-server";
 import { mapProject } from "@/app/lib/mapProject";
 import { notFound } from "next/navigation";
@@ -6,10 +6,12 @@ import { notFound } from "next/navigation";
 export const dynamic = "force-dynamic";
 
 export async function GET(
-  _req: Request,
-  { params }: { params: { slug: string } }
+  _req: NextRequest,
+  { params }: { params: Promise<{ slug: string }> }
   
 ) {
+  const {slug} = await params;
+
   if (!slug || slug === "undefined") {
     notFound();
   }
@@ -22,7 +24,7 @@ export async function GET(
       project_details ( content, demo_url, repo_url ),
       project_tech ( tech )
     `)
-    .eq("slug", params.slug)
+    .eq("slug", (await params).slug)
     .single();
 
   if (error || !data) {
